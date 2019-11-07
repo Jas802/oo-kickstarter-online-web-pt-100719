388b71a8737aa3c15a37a36ffff271b581dca6e0
Jas Spencer <jas.spencer414@gmail.com>
2019-11-07 01:56:43 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+class Project
+  attr_reader :name, :backers
+
+  def initialize(name)
+    @name = name
+    @backers = []
+  end
+
+  def add_backer(backer)
+    backers << backer
+    backer.backed_projects << self
+  end
+
+end
+    
